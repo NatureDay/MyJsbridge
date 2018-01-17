f09567d842e8d5648932dafa25f0ab4738ffccc0
Nature_Day <1004934782@qq.com>
2018-01-17 14:06:15 +0800

    init project

--- jsbridge/src/main/java/com/qianmo/jsbridge/Callback.java
+++ jsbridge/src/main/java/com/qianmo/jsbridge/Callback.java
@@ -1,7 +1,5 @@
 package com.qianmo.jsbridge;
 
-import android.os.Handler;
-import android.os.Looper;
 import android.webkit.WebView;
 
 import org.json.JSONObject;
@@ -14,8 +12,9 @@ import java.lang.ref.WeakReference;
  * Time:   2017/9/26 16:45
  */
 public class Callback {
-    private static Handler mHandler = new Handler(Looper.getMainLooper());
+
     private static final String CALLBACK_JS_FORMAT = "javascript:androidBridge.callback('%s', %s);";
+
     private String mCallbackId;
     private WeakReference<WebView> mWebViewRef;
 
@@ -27,13 +26,7 @@ public class Callback {
     public void apply(JSONObject jsonObject) {
         final String execJs = String.format(CALLBACK_JS_FORMAT, mCallbackId, jsonObject.toString());
         if (mWebViewRef != null && mWebViewRef.get() != null) {
-            mHandler.post(new Runnable() {
-                @Override
-                public void run() {
-                    mWebViewRef.get().loadUrl(execJs);
-                }
-            });
+            mWebViewRef.get().loadUrl(execJs);
         }
-
     }
 }
